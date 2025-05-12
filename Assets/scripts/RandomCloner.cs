e00419c452ff25c8ae0ec4fa71dfcfae13388b02
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-05-12 10:47:53 -0300

    .

---
+++ Assets/scripts/RandomCloner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public int randomNumber;
+    public int min = 1;
+    public int max = 3;
+    public GameObject prefab1;
+    public GameObject prefab2;
+    public GameObject prefab3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloneRandom();
+    }
+
+    public void CloneRandom()
+    {
+        randomNumber = Random.Range(min, max + 1);
+        GameObject clone;
+        if (randomNumber == 0)
+        {
+            clone = prefab1;
+        }
+        else if (randomNumber == 1)
+        {
+            clone = prefab2;
+        }
+        else if (randomNumber == 2)
+        {
+            clone = prefab3;
+        }
+        else
+        {
+            clone = prefab1;
+        }
+        Instantiate(clone);
+    }
+
+}
