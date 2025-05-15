ba611d76c7f9660cc109c74e99d744a69919b520
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-05-15 10:43:19 -0300

    fps

---
+++ Assets/scripts/DeadRespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadRespawn : MonoBehaviour
+{
+    public float deathHeight = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < deathHeight)
+        {
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPSparkour");
+
+        }
+    }
+}
