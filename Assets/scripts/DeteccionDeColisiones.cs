23bac0262d7f02b56d23ba6a9eae10ede4a45b10
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-05-22 10:41:16 -0300

    colision

---
+++ Assets/scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        if(collision.gameObject.name == "Player")
+        {
+        Destroy(gameObject);
+        }
+    }
+
+}
