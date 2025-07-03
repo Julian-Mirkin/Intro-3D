e3205c956ec066011c8058e731e69901aea087a4
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-07-03 10:43:13 -0300

    E

---
+++ Assets/scripts/InteractionArea.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIInteractionMessague;
+    public bool canInteract;
+    public MercaderiaScript mercaderia;
+
+    private void Start()
+    {
+        UIInteractionMessague.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canInteract)
+            {
+                Destroy(mercaderia.gameObject);
+                EndInteraction();
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        mercaderia = other.GetComponent<MercaderiaScript>();
+        if (mercaderia)
+        {
+            UIInteractionMessague.SetActive(true);
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UIInteractionMessague.SetActive(false);
+        canInteract = false;
+        mercaderia = null;
+    }
+        
+    void EndInteraction()
+    {
+        UIInteractionMessague.SetActive(false);
+        canInteract = false;
+        mercaderia = null;
+    }
+}
